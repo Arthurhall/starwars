35d8bd3076ec58d77e60bd0ed5ae025abad5f49f
Arthurhall <arthur.hall@orange.fr>
2019-09-23 08:23:01 +0200

    improve

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 require('../css/dashboard.scss');
+require('../css/flag-icon.scss');
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
